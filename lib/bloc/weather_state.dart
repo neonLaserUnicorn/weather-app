d974e92ad2859ad6cd53f3ed7c97bae13b9535ab
neonLaserUnicorn <astrargot@gmail.com>
2024-05-19 12:35:50 +0300

    Add weatherBloc

---
+++ lib/bloc/weather_state.dart
@@ -0,0 +1,16 @@
+part of 'weather_bloc.dart';
+
+sealed class WeatherState extends Equatable {
+  const WeatherState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class WeatherInitial extends WeatherState {}
+
+final class WeatherLoading extends WeatherState {}
+
+final class WeatherError extends WeatherState {}
+
+final class WeatherSuccess extends WeatherState {}
