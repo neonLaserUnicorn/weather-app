ba081b412c921874fe3e580bdcdbd833acbbd941
neonLaserUnicorn <astrargot@gmail.com>
2024-05-19 16:17:58 +0300

    refactor text in MainWeatherWidget add time text

--- lib/widgets/main_weather_widget.dart
+++ lib/widgets/main_weather_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_icons/weather_icons.dart';
 
 import '../bloc/weather_bloc.dart';
@@ -9,10 +10,12 @@ class MainWeatherWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final timeForm = DateFormat('Hms');
     return SizedBox(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       child: BlocBuilder<WeatherBloc, WeatherState>(
+        buildWhen: (previous, current) => current is WeatherSuccess,
         builder: (context, state) {
           if (state is WeatherSuccess) {
             return Column(
@@ -26,32 +29,16 @@ class MainWeatherWidget extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Center(
-                    child: Text(
+                  _mainText(
                       '${state.position.latitude}, ${state.position.longitude}',
-                      style: const TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      '${state.weather.temperature!.celsius!.round()} °C',
-                      style: const TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                  ),
-                  Center(
-                      child: Text(
-                    state.weather.weatherDescription!.toUpperCase(),
-                    style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                  )),
+                      10),
+                  _mainText(
+                      '${state.weather.temperature!.celsius!.round()} °C', 40),
+                  _mainText(
+                      state.weather.weatherDescription!.toUpperCase(), 30),
+                  _mainText(
+                      'last updated: ${timeForm.format(state.weather.date!)}',
+                      10)
                 ]);
           } else {
             return const Center(
@@ -59,6 +46,16 @@ class MainWeatherWidget extends StatelessWidget {
             );
           }
         },
+      ),
+    );
+  }
+
+  Center _mainText(String message, double size) {
+    return Center(
+      child: Text(
+        message,
+        style: TextStyle(
+            fontSize: size, fontWeight: FontWeight.bold, color: Colors.black),
       ),
     );
   }
